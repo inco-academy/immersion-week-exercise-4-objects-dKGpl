bb55a4b563fe545a1579ac7f3bf973def1ad1968
darekgrabowski <dariuszgrabowski@mixbudkg.pl>
2022-02-05 04:16:14 +0100

    Moddified array init

--- src/4_translate.js
+++ src/4_translate.js
@@ -1,7 +1,7 @@
 module.exports = function translate(inObj, inText) {
-  const textArr = inText.split(" ")
-  let translatedArr = [],
-      err = false
+  const textArr = inText.split(" "),
+        translatedArr = []
+  let err = false
   for(word of textArr){
       Object.keys(inObj).includes(word) ? translatedArr.push(inObj[word]) : err = true
   }
